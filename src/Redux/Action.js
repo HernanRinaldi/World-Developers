b601b387c685da8319f01010ad31ad5855b139a7
Patricio Uskaer <patriciouskaer@gmail.com>
2022-10-11 10:49:10 -0300

    Redux/Action

--- src/Redux/Action.js
+++ src/Redux/Action.js
@@ -1,5 +1,13 @@
 
 //-------------------IMPORTS------------------//
 import axios from 'axios';
+import { getAllHotels, getHotelsById } from './hotelsSlice';
+
+export function getHotels(){
+    return async function(dispatch){
+        let hotels = await axios.get('')
+        dispatch(getAllHotels(hotels.data.results))
+    }
+}
 
 
