74fd7aaff7489116affea645fb934ed4157ad47a
Patricio Uskaer <patriciouskaer@gmail.com>
2022-10-11 10:56:09 -0300

    Redux/Store

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,3 +1,14 @@
 
 //------------IMPORTS--------------------//
- 
+ import { configureStore } from "@reduxjs/toolkit";
+ import hotels from "./hotelsSlice";
+ import detail from "./hotelsSlice";
+
+
+ export default configureStore({
+    reducer:{
+        hotels: hotels,
+        detail: detail
+       // rooms: rooms 
+    }
+ })
