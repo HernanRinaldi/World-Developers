a95871441d366229179b9ab7f9cb8c3575de547f
Patricio Uskaer <patriciouskaer@gmail.com>
2022-10-13 13:42:54 -0300

    Search + Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CardHotels from '../CardHotels/CardHotels';
+import Search from '../Search/Search';
 
 
 const Home = () => {
   return (
     <div>
+        <Search/>
         <CardHotels/>
     </div>
   )
