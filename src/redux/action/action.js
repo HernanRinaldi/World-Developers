078f55b957310ecd740b30fd07e67e5839f45a05
TheOliver413 <oliverchemas@hotmail.com>
2022-10-13 21:06:11 -0500

    Nuevas actios

--- src/redux/action/action.js
+++ src/redux/action/action.js
@@ -3,15 +3,47 @@ import { dataBase } from '../../dataBase/dataBase';
 
 export const GET_ALL_HOTELS = "GET_ALL_HOTELS";
 export const GET_ALL_ROOMS = "GET_ALL_ROOMS";
-export const SEARCH_NAME = "SEARCH_NAME";
+export const SEARCH_NAME_HOTEL = "SEARCH_NAME_HOTEL";
+export const SEARCH_NAME_ROOM = "SEARCH_NAME_ROOM";
 export const GET_HOTEL_DETAIL = "GET_HOTEL_DETAIL";
 export const GET_ROOMS_DETAIL = "GET_ROOMS_DETAIL";
 export const CLEAR_HOTEL_DETAIL = "CLEAR_HOTEL_DETAIL";
 export const CLEAR_ROOM_DETAIL = "CLEAR_ROOM_DETAIL";
 export const GET_SERVICE_HOTEL = "GET_SERVICE_HOTEL";
+export const POST_HOTEL = "POST_HOTEL";
+export const POST_ROOM = "POST_ROOM";
+
 
 const BACK_URL = "http://localhost:3001"
 //----------------------- HOTELS ------------------------------//
+
+export function postHotel(payload){
+  return async function(dispatch) {
+    try {
+      const hotels = await axios.post(`${BACK_URL}/hotels`,payload)
+      return dispatch ({
+        type: POST_HOTEL,
+        payload: hotels
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export function getDetailHotel(id){
+  return async function(dispatch) {
+    try {
+      const detailH = await axios.get(`${BACK_URL}/hotels/${id}`)
+      return dispatch({
+        type: GET_HOTEL_DETAIL, 
+        payload: detailH.data
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
 
 export function getHotels() {
   return async function (dispatch) {
@@ -23,23 +55,13 @@ export function getHotels() {
   }
 }
 
-export function getRooms() {
-  return async function (dispatch) {
-    let rooms = await axios.get(`${BACK_URL}/rooms`)
-    dispatch({
-      payload: rooms.data
-    })
-  }
-}
-
-
 export function hotelByName(name) {
   return async function (dispatch) {
     try {
       if (name) {
         let hotelName = await axios.get (`${BACK_URL}/hotels?name=${name}`)
         return dispatch({
-          type: SEARCH_NAME,
+          type: SEARCH_NAME_HOTEL,
           payload: hotelName.data
         })
       }
@@ -49,30 +71,58 @@ export function hotelByName(name) {
   }
 }
 
-export function hotelById(id) {
-  return async function (dispatch) {
-     try {
-       let hotelDetail = await axios.get(`${BACK_URL}/hotels/${id}`)
-       dispatch({
-         type: GET_HOTEL_DETAIL,
-         payload: hotelDetail.data
-       })
-     } catch (error) {
-       console.log(error)
-     }
-   }
+export function postRoom(payload){
+  return async function(dispatch) {
+    try {
+      const rooms = await axios.post(`${BACK_URL}/rooms`,payload)
+      dispatch ({
+        type: POST_ROOM,
+        payload: rooms
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
 
-export function roomById(id) {
-   return async function (dispatch) {
-     try {
-       let roomDetail = await axios.get(`${BACK_URL}/rooms/${id}`)
-       dispatch({
-         type: GET_ROOMS_DETAIL,
-         payload: roomDetail.data
-       })
-     } catch (error) {
-       console.log(error)
-     }
-   }
+export function getDetailRoom(id){
+  return async function(dispatch) {
+    try {
+      const detailR = await axios.get(`${BACK_URL}/rooms/${id}`)
+      return dispatch({
+        type: GET_ROOMS_DETAIL, 
+        payload: detailR.data
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
+}
+
+export function getRooms() {
+  return async function (dispatch) {
+    let rooms = await axios.get(`${BACK_URL}/rooms`)
+    dispatch({
+      type: GET_ALL_ROOMS,
+      payload: rooms.data
+    })
+  }
+}
+
+export function roomByName(name) {
+  return async function (dispatch) {
+    try {
+      if (name) {
+        let roomName = await axios.get (`${BACK_URL}/rooms?name=${name}`)
+        return dispatch({
+          type: SEARCH_NAME_ROOM,
+          payload: roomName.data
+        })
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+
